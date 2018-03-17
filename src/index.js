cf3a742311c09c5d4da906dbc25b7758f91d02c5
wsavd <tuve@rootfest.net>
2018-03-17 13:50:23 +0300

    slider (#task2)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react"; 
 import { render } from "react-dom";
 import axios from "axios";
+import "antd/dist/antd.css";
+import { Slider } from "antd";
 
 export class App extends Component {
+
   state = {
     studios: [],
     isLoading: false,
@@ -12,27 +15,63 @@ export class App extends Component {
   componentDidMount() {
     this.setState({ isLoading: true });
 
-    axios
-      .get("https://5aa59009f207ec00144acd27.mockapi.io/api/v1/studios")
+    axios.get("https://5aa59009f207ec00144acd27.mockapi.io/api/v1/studios")
       .then(res => {
-        this.setState({
-          studios: res.data,
-          isLoading: false,
-        });
+
+        const min = res.data.reduce(
+            (acc, el) => (acc > el.price ? el.price : acc),
+            Infinity
+          );
+        const max = res.data.reduce(
+        (acc, el) => (acc < el.price ? el.price : acc),
+            -Infinity
+          );
+
+          this.setState({
+            studios: res.data,
+            isLoading: false,
+            min,
+            max,
+            from: min,
+            to: max
+          });
+
       })
+
       .catch(err => this.setState({ loading: false, error: err }));
   }
 
+  onChange = value => {
+    const [from, to] = value;
+
+    this.setState({
+      from,
+      to
+    });
+  };
+
   render() {
-    const { studios, isLoading, error } = this.state;
+    const { studios, isLoading, error, min, max, from, to } = this.state;
 
     if (isLoading) return <div className="spinner"></div>;
     if (error) return <div>{error.message}</div>;
+
+    const filteredStudios = studios.filter(
+        studio => studio.price >= from && studio.price <= to
+    )
     
     return (
       <div className="app">
 
-            {studios.map(studio =>
+            <div className="slider">
+                <div className="slider__container">
+                  <div>Стоимость</div>
+                  <div>от {from} до {to}</div>
+                </div>
+                <Slider range min={min} max={max} defaultValue={[1100, 2200]} onChange={this.onChange} />
+            </div>
+
+            {filteredStudios.map(studio =>
               <div key={studio.id} className="studio">
                 <div>{studio.name}</div>
               </div>
